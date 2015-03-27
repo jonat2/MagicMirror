9cd32bdfbf9ebbe4ead2b68bea80fe6b42057ad9
jonat2 <jonat2@hotmail.com>
2015-03-27 14:23:01 -0400

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <?php
 	
 	// Set the url of the calendar feed.
-	$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
+	$url = 'p25-calendarws.icloud.com/ca/subscribe/1/amrrW7kBXM5Nlifb6mY3Zq9FFrpH0VL3U_WIWDk1n21lpqIkvVxoDTQH7JbBCK1G';
 
 	/*****************************************/
 
